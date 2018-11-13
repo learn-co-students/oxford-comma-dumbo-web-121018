36b3dd1709e59d3ac29745fa1e2f03c5022edbfd
Kah Yap <kahyap13@gmail.com>
2018-11-13 18:31:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  string = ""
+  counter = 0
+  while counter < array.length
+    if counter == 0
+      string += array[0]
+    elsif array.length == 2 && counter == 1
+      string += " and #{array[counter]}"
+    elsif counter == array.length - 1
+      string += ", and #{array[counter]}"
+    else
+      string += ", #{array[counter]}"
+    end
+    counter += 1
+  end
+  return string
 end
