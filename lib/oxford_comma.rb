68f6f7dafd794c1d7bed26d8a1b1bf3522c0e3cf
Menachem Aber <abermendel@gmail.com>
2018-11-23 03:23:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join
+  when 2
+    array = array.insert(-2, "and")
+    array = array.join(" ")
+    array
+  else
+    array = array.insert(-2, "and")
+    for i in (0...array.length-2) do
+      array[i] += ","
+    end
+    array = array.join(" ")
+    array
+  end
 end
