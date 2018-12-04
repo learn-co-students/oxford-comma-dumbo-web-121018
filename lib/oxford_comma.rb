69bac9d38f3e02b3e43e1f35f6bff234e094b0b1
jesusg1012 <jesus.jg.medina@gmailcom>
2018-12-04 06:59:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    stringer = " and "
+  elsif array.length > 2
+    stringer = ", and "
+  else
+    stringer = ""
+  end
+  name = array.pop
+  string = array.join(", ")
+  string += stringer
+  string += "#{name}"
+  return string
 end
