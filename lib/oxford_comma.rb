d737c60d6900fc38bf49650903a7790d2bea1805
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-06 07:18:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(' and ')
+  elsif array.length > 2
+    last_words = "and #{array[-1]}"
+    array.pop
+    array.push(last_words)
+    return array.join(', ')
+  end
 end
